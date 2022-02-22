0382e667747bcf40f89080efb26a61f6c6126360
Dini4 <portyanko.denis@mail.ru>
2022-02-22 16:43:09 +0300

    in

---
+++ js/jsFox.js
@@ -0,0 +1,21 @@
+const button = document.querySelector(".btn");
+const image = document.querySelector(".img");
+const url = "https://randomfox.ca/floof/";
+
+async function detchHandler(){
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+
+        image.src = data.image;
+    } catch (error){
+        console.log(error)
+    }
+}
+
+button.addEventListener("click", () =>{
+    let isloaded = image.complete;
+    if(isloaded){
+        detchHandler();
+    }
+})
